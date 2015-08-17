f11c1b588336ca07273b0e61f6e10cbc5eaad39d
Andrew Heekin <andrewheekin@gmail.com>
2015-08-17 09:41:34 -0400

    made names clearer, fixed old variable name

--- csv2flare.json.py
+++ csv2flare.json.py
@@ -15,12 +15,12 @@ df = pd.read_csv('path/to/csv/file.csv')
 
 
 # choose columns to keep, in the desired nested json hierarchical order
-df = df[["keep_me", "and_me", "me_too"]]
+df = df[["the_parent", "the_child", "child_size"]]
 
 
 # order in the groupby here matters, it determines the json nesting
 # the groupby call makes a pandas series by grouping 'keep_me' and 'and_me', while summing the numerical column 'me_too'
-df1 = df.groupby(['keep_me', 'and_me'])['me_too'].sum()
+df1 = df.groupby(['the_parent', 'the_child'])['child_size'].sum()
 df1 = df1.reset_index()
 
 
@@ -30,9 +30,9 @@ flare = {"name":"flare", "children": []}
 
 
 for line in df1.values:
-    keep_me = line[0]
-    and_me = line[1]
-    me_too = line[2]
+    the_parent = line[0]
+    the_child = line[1]
+    child_size = line[2]
 
     # make a list of keys
     keys_list = []
@@ -41,11 +41,11 @@ for line in df1.values:
 
     # if 'keep_me' is NOT a key in the flare.json yet, append it
     if not keep_me in keys_list:
-        d['children'].append({"name":keep_me, "children":[{"name":and_me, "size":me_too}]})
+        d['children'].append({"name":the_parent, "children":[{"name":the_child, "size":child_size}]})
 
     # if 'keep_me' IS a key in the flare.json, add a new child to it
     else:
-        d['children'][districtslist.index(keep_me)]['children'].append({"name":and_me, "size":me_too})
+        d['children'][keys_list.index(the_parent)]['children'].append({"name":the_child, "size":child_size})
 
 flare = d
 
